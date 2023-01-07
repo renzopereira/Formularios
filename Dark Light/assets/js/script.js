36b4a94740d208ef1d509e31a020affe6e3c6ed3
Renzo Pereira <pereirarenzodev@gmail.com>
2023-01-07 15:48:48 -0300

    Atualização

--- Dark Light/assets/js/script.js
+++ Dark Light/assets/js/script.js
@@ -2,11 +2,13 @@ const mode = document.getElementById('mode_icon');
 
 mode.addEventListener('click', () => {
     const form = document.getElementById('login');
+    const main = document.getElementById('container');
     if (mode.classList.contains('fa-moon')) {
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
 
         form.classList.add('dark');
+        main.classList.add('dark');
         return;
     }
 
